41a9b277b7bef5fe258405e4e3e46cae0ee4cbd3
Aditya <adityasingh220598@gmail.com>
2023-03-26 09:17:54 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import {
+  Container,
+  TextField,
+  Button,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const handleNewMessage = (msg) => {
+      setMessages((messages) => [...messages, msg]);
+    };
+    socket.on("message", handleNewMessage);
+
+    // Cleanup the event listener
+    return () => {
+      socket.off("message", handleNewMessage);
+    };
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit("message", message);
+    setMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <List>
+        {messages.map((msg, i) => (
+          <ListItem key={i}>
+            <ListItemText primary={msg} />
+          </ListItem>
+        ))}
+      </List>
+      <form onSubmit={sendMessage}>
+        <TextField
+          label="Type your message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          fullWidth
+          margin="normal"
+          variant="outlined"
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Send
+        </Button>
+      </form>
+    </Container>
   );
 }
 
